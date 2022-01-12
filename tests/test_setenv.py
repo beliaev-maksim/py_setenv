56b60fc0f1d51f7df42e32028699d293cf9ab024
Superuser-Raccoon <50605888+Raccon123@users.noreply.github.com>
2022-01-12 14:56:35 -0600

    This might work

--- tests/test_setenv.py
+++ tests/test_setenv.py
@@ -41,7 +41,7 @@ class TestSetEnv(unittest.TestCase):
 
         # test that new text was appended
         result = self.runner.invoke(click_command, ["unittest", "-v", "appended_str", "-a"] + user_list)
-        self.assertRaises(KeyError, result.output)
+        self.assertRaises(KeyError, result)
 
         result = self.runner.invoke(click_command, ["unittest"] + user_list)
         self.assertEqual("test_str;appended_str", result.output.strip())
