10581eaac5cccf0fb7a083c4c30d0d60c86c2cbe
Superuser-Raccoon <50605888+Raccon123@users.noreply.github.com>
2022-01-12 20:29:07 -0600

    update This might work

--- tests/test_setenv.py
+++ tests/test_setenv.py
@@ -24,6 +24,7 @@ class TestSetEnv(unittest.TestCase):
     def run_checks(self, user):
         # check that variable does not exist before start
         user_list = ["--user"] if user else []
+        
         result = self.runner.invoke(click_command, ["unittest"] + user_list)
         self.assertEqual("Environment Variable 'unittest' does not exist", result.output.strip())
 
@@ -43,6 +44,7 @@ class TestSetEnv(unittest.TestCase):
         result = self.runner.invoke(click_command, ["unittest", "-v", "appended_str", "-a"] + user_list)
         self.assertRaises(KeyError, result)
 
+        #
         result = self.runner.invoke(click_command, ["unittest"] + user_list)
         self.assertEqual("test_str;appended_str", result.output.strip())
 
